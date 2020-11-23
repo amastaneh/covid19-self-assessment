bfa3a94976aa272b7595e30597bc67732555ddcf
Amir Astaneh <amastaneh@storagevaultcanada.com>
2020-11-23 17:15:35 -0500

    Add i18n for localization

--- js/index.js
+++ js/index.js
@@ -1,14 +1,27 @@
-// var swiper = new Swiper('.blog-slider', {
-//     spaceBetween: 30,
-//     effect: 'fade',
-//     loop: true,
-//     mousewheel: {
-//         invert: false,
-//     },
-//     // autoHeight: true,
-//     pagination: {
-//         el: '.blog-slider__pagination',
-//         clickable: true,
-//     }
-// });
-//var swiper = new Swiper('.blog-slider');
+var updateLanguage = function(locale) {
+    if (locale) {
+        $.i18n().locale = this.value;
+    }
+    $('body').i18n();
+};
+
+$(document).ready(function($) {
+    'use strict';
+    $.i18n({
+        //locale: 'en'
+    }).load({
+        en: 'js/i18n/en.json',
+        fr: 'js/i18n/fr.json'
+    }).done(function() {
+        updateLanguage(url('?hl'));
+        History.Adapter.bind(window, 'statechange', function() {
+            updateLanguage(url('?hl'));
+        });
+        // Language combobox
+        $('#switch-locale').on('change', function(e) {
+            e.preventDefault();
+            History.pushState(null, null, "?hl=" + this.value);
+        });
+        updateLanguage();
+    });
+});
